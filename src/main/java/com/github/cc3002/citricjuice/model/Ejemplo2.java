ea9aa3d1624d71206e278e37eb214de502260e6d
David Parada <davidparada@ug.uchile.cl>
2021-04-16 13:38:51 -0400

    Commit desde IntelliJ

---
+++ src/main/java/com/github/cc3002/citricjuice/model/Ejemplo2.java
@@ -0,0 +1,7 @@
+package com.github.cc3002.citricjuice.model;
+
+public class Ejemplo2 {
+    public static void main(String[] args){
+        System.out.println("Ejemplo 2, Commit desde IntelliJ");
+    }
+}
